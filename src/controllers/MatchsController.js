56722215f2ae90e68e89e7045e1ca09daa50356e
Ítalo Peruchi <itl95pps@gmail.com>
2020-01-30 15:26:27 -0300

    Update MatchsController.js

--- src/controllers/MatchsController.js
+++ src/controllers/MatchsController.js
@@ -3,18 +3,11 @@ const Dev = require('../models/Dev');
 module.exports = {
   async index(req, res) {
     const { user } = req.headers;
-    const { devId } = req.params;
-
-    const loggedDev = await Dev.findById(user);
-
-    const users = await Dev.find({
-      $and: [
-        { _id: { $ne: user } },
-        {likes: { $in: devId } },
-        {dislikes: { $nin: devId } }
-      ],
-    });
-
-    return res.json(users);
+    const userLogged = await Dev.findById(user);
+    const usersILiked = await Dev.find(
+      { _id: { $in: userLogged.likes } }
+    );
+    const matchs = usersILiked.filter(x=> x.likes.includes(userLogged._id))
+    return res.json(matchs);
   }
 }
