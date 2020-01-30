d155db19f83b5a294e03686496c4ef4185958744
Ítalo Peruchi <itl95pps@gmail.com>
2020-01-30 15:23:46 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -12,6 +12,6 @@ routes.post('/devs', DevController.store);
 routes.post('/devs/:devId/likes', LikeController.store);
 routes.post('/devs/:devId/dislikes', DislikeController.store);
 
-routes.get('/devs/:devId/matchs', MatchsController.index);
+routes.get('/devs/matchs', MatchsController.index);
 
 module.exports = routes;
